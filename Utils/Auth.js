7eaa2454283609feeb5ce83d2d8bb1649011ac53
Muhammad Shakeel <149057002+shakeel7521951@users.noreply.github.com>
2025-09-04 17:40:14 +0500

    Update Auth.js

--- Utils/Auth.js
+++ Utils/Auth.js
@@ -3,17 +3,25 @@ import UserModel from '../Model/UserModel.js';
 import jwt from 'jsonwebtoken';
 
 export const isUserLoggedin = async (req, res, next) => {
-  const { token } = req.cookies;
-
-  if (!token) {
-    return next(new Errorhandler('Please login to access this page.'));
+  try {
+    let token;
+    if (req.cookies?.token) {
+      token = req.cookies.token;
+    }
+    else if (req.headers?.authorization && req.headers.authorization.startsWith("Bearer ")) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+    if (!token) {
+      return next(new Errorhandler("Please login to access this page.", 401));
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await UserModel.findById(decoded.id);
+    if (!user) {
+      return next(new Errorhandler("User not found.", 404));
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    return next(new Errorhandler("Invalid or expired token.", 401));
   }
-
-  const Decode = jwt.verify(token, process.env.JWT_SECRET);
-
-  const user = await UserModel.findById(Decode.id);
-
-  req.user = user;
-
-  next();
 };
