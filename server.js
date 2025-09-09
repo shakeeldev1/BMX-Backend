d6d42716508d141207d661f4f736ed25d70d82ef
Muhammad Shakeel <shakeel7521951@gmail.com>
2025-09-09 13:00:10 +0500

    update origin

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ app.use(express.json());
 
 app.use(
   cors({
-    // origin: "https://bmx-frontend.vercel.app",
-    origin: "http://localhost:5174",
+    origin: "https://bmx-frontend.vercel.app",
+    // origin: "http://localhost:5174",
     credentials: true,
   })
 );
